de13ba9fcde202a2d7da1ed53ca9e07c9c41c223
RochaGabriell <gabrielrocha1902@gmail.com>
2022-09-12 00:37:03 -0300

    Fake_Data

---
+++ Fake_Data/main.py
@@ -0,0 +1,17 @@
+from faker import Faker
+
+fake = Faker()
+
+data_dictionary = []
+
+for cont in range(0, 10):
+    data_dictionary.append({
+        "name" : fake.name(),
+        "first_name" : fake.first_name(),
+        "user" : fake.user_name(),
+        "password" : fake.password(),
+        "month" : fake.month()
+    })
+
+for cont in data_dictionary:
+    print(cont)
